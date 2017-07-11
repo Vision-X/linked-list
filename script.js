ce87c6b6ae6b92cad6e4e4dd92c0f688ffe91b72
Rufus <rufusasterisk@gmail.com>
2017-07-10 20:37:11 -0600

    Add card construction js

---
+++ script.js
@@ -0,0 +1,27 @@
+function addArticle(userTitle, userURL){
+  var newArticle = document.createElement("article");
+  var newH2 = document.createElement("h2");
+  var newA = document.createElement("a");
+  var newReadButton = document.createElement("button");
+  var newDeleteButton = document.createElement("button");
+
+  newArticle.appendChild(newH2);
+  newArticle.appendChild(document.createElement('hr'));
+  newArticle.appendChild(newA);
+  newArticle.appendChild(document.createElement('hr'));
+  newArticle.appendChild(newReadButton);
+  newArticle.appendChild(newDeleteButton);
+
+  newArticle.classList.add('card');
+  newH2.innerHTML = userTitle;
+  newA.innerHTML = userURL;
+  newA.href = userURL;
+  newReadButton.innerHTML = "Read";
+  newReadButton.classList.add("readButton");
+  newDeleteButton.innerHTML = "Delete";
+  newDeleteButton.classList.add("deleteButton");
+
+  var currentArticle = document.getElementById('card1');
+  var sectionRight = document.querySelector('.section-right');
+  sectionRight.insertBefore(newArticle, currentArticle);
+}
