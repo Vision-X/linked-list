0c0fecce2fd692a1b0bd141d376afa3901388a15
Rufus <rufusasterisk@gmail.com>
2017-07-13 08:32:15 -0600

    Add url repair, Add clear btn disable, Change css to apply disabled style to both buttons

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ $("#enter-btn").on('click', function() {
 $('.card-section').on('click', '.readbtn', function() {
   $(this).parent().toggleClass("read");
   updateReadCounters();
+  if($('.read').length === 0){
+    console.log('should disable');
+    $("#clear-read-btn").attr('disabled', true);
+  }
+  else{
+    console.log('should enable');
+    $("#clear-read-btn").attr('disabled', false);
+  }
 })
 
 $('.card-section').on('click', '.deletebtn', function() {
@@ -33,11 +41,17 @@ $("#clear-read-btn").on('click', function(){
 function createBookmark() {
   var title = $(".input-field-1").val();
   var url = $(".input-field-2").val();
+  if(/^(http[s]?:\/\/)/.test(url)){
+    var pathURL = url;
+  }
+  else {
+    var pathURL = "http://" + url;
+  }
   var placeholder = `
   <article class="card">
     <h2>${title}</h2>
     <hr>
-    <a target = _blank href="${url}">${url}</a>
+    <a target = _blank href="${pathURL}">${url}</a>
     <hr>
     <button type="button" class="readbtn">Read</button>
     <button type="button" class="deletebtn">Delete</button>
