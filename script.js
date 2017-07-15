9d3ce2c2940c250117ffbfbed03db2ae6183e594
Mike Marlow <michael.s.marlow@gmail.com>
2017-07-15 14:28:26 -0600

    yeah idk

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //on page load
 checkForInput();
+toggleReadButton();
 
 //listeners
 $("#enter-btn").on('click', function() {
@@ -9,6 +10,7 @@ $("#enter-btn").on('click', function() {
 $('.card-section').on('click', '.readbtn', function() {
   $(this).parent().toggleClass("read");
   updateReadCounters();
+  toggleReadButton();
 })
 
 $('.card-section').on('click', '.deletebtn', function() {
@@ -33,11 +35,17 @@ $("#clear-read-btn").on('click', function(){
 function createBookmark() {
   var title = $(".input-field-1").val();
   var url = $(".input-field-2").val();
+  if(/^(http[s]?:\/\/)/.test(url)){
+    var pathURL = url;
+  }
+  else {
+    var pathURL = "http://" + url;
+  }
   var placeholder = `
   <article class="card">
     <h2>${title}</h2>
     <hr>
-    <a target = _blank href="${url}">${url}</a>
+    <a target = _blank href="${pathURL}">${url}</a>
     <hr>
     <button type="button" class="readbtn">Read</button>
     <button type="button" class="deletebtn">Delete</button>
@@ -73,4 +81,14 @@ function updateReadCounters() {
   $('#read-display').text(currentReadCount);
   var currentUnreadCount = $('.card').length - $('.card.read').length;
   $('#unread-display').text(currentUnreadCount);
+}
+function toggleReadButton(){
+  if($('.read').length === 0){
+    console.log('should disable');
+    $("#clear-read-btn").attr('disabled', true);
+  }
+  else{
+    console.log('should enable');
+    $("#clear-read-btn").attr('disabled', false);
+  }
 }
